5fd59b2b052183e3ddd86f51901fb97e15fdc230
Alyona Arlova <alyona_arlova@epam.com>
2025-07-28 20:55:07 +0200

    During Lesson - Python tasks

---
+++ PYTHON/Lesson6DuringLessonPython1.py
@@ -0,0 +1,35 @@
+#example of variables and data types
+myInt=4
+myReal=2.5
+myChar='a'
+"""
+#input from keyboard
+a=int(input("Enter a int number 1: "))
+b=float(input("Enter a float number 2: "))
+c=str(input("Enter a str 3: "))
+print(c)
+myString='hello'
+print(myInt)
+print(myReal)
+print(myChar)
+print(myString)
+print('hello')
+
+#Сложение чисел: Посчитайте и выведите сумму чисел 7 и 15.
+f=int(input("Enter a int number for F: "))
+g=int(input("Enter a int number for G: "))
+c1=f+g
+print(c1)
+
+#Вычислите результат выражения (10 + 5) * 2 и выведите его
+t=int(input("Enter a int number 1: "))
+z=int(input("Enter a int number 2: "))
+p=int(input("Enter a int number 3: "))
+d=(t+z)*p
+print(p)
+"""
+#Найдите остаток от деления числа 17 на 3 и выведите результат
+a=int(input("Enter a int number 1: "))
+b=int(input("Enter a int number 2: "))
+c=a%b
+print(c)
