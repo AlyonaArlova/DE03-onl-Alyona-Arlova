db8e03d7bc563ecb05a69bff0af2b1d89269ecaa
Alyona Arlova <alyona_arlova@epam.com>
2025-07-29 10:53:15 +0200

    During Lesson - Python tasks

--- PYTHON/HW6_Python1.py
+++ PYTHON/HW6_Python1.py
@@ -3,7 +3,7 @@ print('I love (hate?) Python')
 attitudeToPython=str(input("What do you feel about Python(love or hate)?: "))
 print(f'I {attitudeToPython} Python')
 
-#Напишите программу, которая выводит на экран результат сложения двух чисел, например, 3 + 5
+#Напишите программу, которая выводит на экран результат сложения двух чисел
 a=int(input("Enter int number 1: "))
 b=int(input("Enter int number 2: "))
 c=a+b
