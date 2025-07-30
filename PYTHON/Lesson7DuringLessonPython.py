d752cc053a94a0243835ce35c8c97acbd3673b27
Alyona Arlova <alyona_arlova@epam.com>
2025-07-30 21:15:24 +0200

    During Lesson - Python tasks

--- PYTHON/Lesson7DuringLessonPython.py
+++ PYTHON/Lesson7DuringLessonPython.py
@@ -10,7 +10,7 @@ for letter in word:
 print(f"Количество гласных: {count}")
 
 #Проверка четности числа
-a=int(input("Enter number: "))
+a = int(input("Enter number: "))
 if a % 2 == 0:
     print("Число четное")
 else:
@@ -18,9 +18,9 @@ else:
 
 #Напишите программу, которая находит наибольшее из трех чисел: 3, 7 и 5.
 # Программа должна выводить максимальное число.
-a=int(input("Enter number 1: "))
-b=int(input("Enter number 2: "))
-c=int(input("Enter number 3: "))
-max_number= max(a,b,c)
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+c = int(input("Enter number 3: "))
+max_number = max(a,b,c)
 print(max_number)
 
